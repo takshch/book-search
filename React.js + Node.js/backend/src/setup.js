cadea959670499b74f95f74291046d6931993e3c
takshch <takshch089@gmail.com>
2022-04-17 22:00:27 +0530

    Moves setup to src & Adds setup run in both env

--- React.js + Node.js/backend/src/setup.js
+++ React.js + Node.js/backend/src/setup.js
@@ -1,6 +1,6 @@
-const { createTable } = require('./src/utils/create-table');
-const { listTables } = require('./src/utils/list-tables');
-const { Schema: BOOK_SEARCH_SCHEMA } = require('./src/schema/create-book-table');
+const { createTable } = require('./utils/create-table');
+const { listTables } = require('./utils/list-tables');
+const { Schema: BOOK_SEARCH_SCHEMA } = require('./schema/create-book-table');
 
 listTables().then(res => {
   const doesBookSearchTableExists = res.TableNames.includes('BOOK_SEARCH');
