5826a09d177eeffbf2e6cec3135ab0d1d701b72d
takshch <takshch089@gmail.com>
2022-04-18 22:38:03 +0530

    Updates application-start and application-stop shells script

--- hooks_scripts/application-start.sh
+++ hooks_scripts/application-start.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+sudo chmod -R 777 /home/ubuntu/server
+
 cd home/ubuntu/server
+
+#add npm and node to path
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
 # install node modules
 npm install
