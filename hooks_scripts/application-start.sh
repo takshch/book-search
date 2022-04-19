a0b46ef2495a43fd56cbaa9e4292ad0e3ce74fab
takshch <takshch089@gmail.com>
2022-04-19 19:53:57 +0530

    Deploys Ember.js app instead of React.js + Node.js

--- hooks_scripts/application-start.sh
+++ hooks_scripts/application-start.sh
@@ -2,23 +2,8 @@
 
 sudo chmod -R 777 /home/ubuntu/server
 
-cd "/home/ubuntu/server/React.js + Node.js/"
+cd "/home/ubuntu/server/Ember.js + Firebase/client"
 
-#add npm and node to path
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
-
-# start react.js app
-cd client
-npm install
 npm run build
-sudo pm2 serve build 80 --spa
-pm2 save
-
-# start our node backend
-cd ../backend
-npm install
-npm run setup:prod
-sudo pm2 -f start ecosystem.config.js
+sudo pm2 serve dist 80 --spa
 pm2 save
