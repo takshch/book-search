87b58e59463ded777e309c3d7c49ba180728b704
takshch <takshch089@gmail.com>
2022-04-19 10:44:47 +0530

    Adds uncommitted application-start changes

--- hooks_scripts/application-start.sh
+++ hooks_scripts/application-start.sh
@@ -2,18 +2,24 @@
 
 sudo chmod -R 777 /home/ubuntu/server
 
-cd "/home/ubuntu/server/React.js + Node.js/backend"
+cd "/home/ubuntu/server/React.js + Node.js/"
 
 #add npm and node to path
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
-# install node modules
+
+# start react.js app
+cd client
 npm install
+npm run build
+pm2 serve build 3000 --spa
+pm2 save
 
 # start our node backend
+cd ../backend
+npm install
 npm run setup:prod
 sudo pm2 -f start ecosystem.config.js
-pm2 startup
 pm2 save
