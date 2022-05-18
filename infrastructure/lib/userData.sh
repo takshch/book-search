b5734d2e5508dc1b8cb179d246eef0026af92dda
takshch <takshch089@gmail.com>
2022-05-18 16:09:13 +0530

    Fixes pm2 package install

--- infrastructure/lib/userData.sh
+++ infrastructure/lib/userData.sh
@@ -6,7 +6,7 @@ curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
 sudo yum install -y nodejs
 sudo yum install -y nvm
 
-sudo npm install pm2@latest -y
+sudo npm install -g pm2@latest -y
 
 sudo yum install -y ruby
 sudo yum install -y wget
