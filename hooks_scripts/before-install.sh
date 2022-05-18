8f1f727f267109c5f7c424c3a8826c2b6396956b
takshch <takshch089@gmail.com>
2022-05-18 16:35:56 +0530

    Remove nvm installation from before-install shell script

--- hooks_scripts/before-install.sh
+++ hooks_scripts/before-install.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-#download node and npm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install node
-
 #create our working directory if it doesnt exist
 DIR="$HOME/server"
 if [ -d "$DIR" ]; then
