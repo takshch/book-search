097bd3730a664eeec27a3611276f16ac4eb5dd06
takshch <takshch089@gmail.com>
2022-05-20 15:32:52 +0530

    Fix application-start shell

--- hooks_scripts/application-start.sh
+++ hooks_scripts/application-start.sh
@@ -2,11 +2,11 @@
 
 sudo chmod -R 777 /home/ec2-user/server
 
-cd "/home/ec2-user/server/Ember.js + Firebase/client"
+cd "/home/ec2-user/server/client"
 
 npm install
 npm rebuild node-sass
 
 npm run build
-pm2 serve dist 80 --spa
-pm2 save
+sudo pm2 serve dist 80 --spa
+sudo pm2 save
